06518cb4b4545aff86c336b42619f3a4fc411b3f
sathiyanm <sathiyanm@gmail.com>
2024-07-26 16:18:06 +0530

    updates

--- src/config.ts
+++ src/config.ts
@@ -1,37 +1,37 @@
 import type { PostHideElements } from '~/content/config';
 
-export const Site = 'https://astro-blog-zozo.pages.dev';
+export const Site = '';
 export const SiteLanguage = 'zh';
-export const SiteTitle = '无尽光芒';
-export const SiteDescription = '踏歌长行，梦想永在';
-export const FooterDescription = '肆意闪耀';
-export const AdminName = 'Ladit';
+export const SiteTitle = 'TamilCoder';
+export const SiteDescription = '';
+export const FooterDescription = '';
+export const AdminName = 'sathya';
 export const PageSize = 15;
 
 // socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-	mail: { url: 'mailto:i@ladit.me' },
-	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
-	x: { url: 'https://x.com/userName' },
-	mastodon: { url: 'https://mastodon.social/@userName' },
-	facebook: { url: 'https://facebook.com/userName' },
-	instagram: { url: 'https://instagram.com/userName' },
-	telegram: { url: 'https://t.me/@userName' },
-	youtube: { url: 'https://youtube.com/@userName' },
-	skype: { url: 'https://skype.com/userName' },
-	slack: { url: 'https://slack.com/userName' },
-	messenger: { url: 'https://messenger.com/userName' },
-	whatsapp: { url: 'https://whatsapp.com/userName' },
-	snapchat: { url: 'https://snapchat.com/userName' },
-	line: { url: 'https://line.com/userName' },
-	twitch: { url: 'https://twitch.com/userName' },
-	weibo: { url: 'https://weibo.com/userName' },
-	bilibili: { url: 'https://space.bilibili.com/userName' },
-	dingding: { url: 'https://dingtalk.com/userName' },
-	zhihu: { url: 'https://zhihu.com/userName' },
-	douban: { url: 'https://douban.com/userName' },
-	rss: { url: '/rss.xml' },
+	mail: { url: 'mailto:sathiyanm@gmail.com' },
+	github: { url: 'https://github.com/sathiyanm' },
+	// x: { url: 'https://x.com/userName' },
+	// mastodon: { url: 'https://mastodon.social/@userName' },
+	// facebook: { url: 'https://facebook.com/userName' },
+	// instagram: { url: 'https://instagram.com/userName' },
+	// telegram: { url: 'https://t.me/@userName' },
+	// youtube: { url: 'https://youtube.com/@userName' },
+	// skype: { url: 'https://skype.com/userName' },
+	// slack: { url: 'https://slack.com/userName' },
+	// messenger: { url: 'https://messenger.com/userName' },
+	// whatsapp: { url: 'https://whatsapp.com/userName' },
+	// snapchat: { url: 'https://snapchat.com/userName' },
+	// line: { url: 'https://line.com/userName' },
+	// twitch: { url: 'https://twitch.com/userName' },
+	// weibo: { url: 'https://weibo.com/userName' },
+	// bilibili: { url: 'https://space.bilibili.com/userName' },
+	// dingding: { url: 'https://dingtalk.com/userName' },
+	// zhihu: { url: 'https://zhihu.com/userName' },
+	// douban: { url: 'https://douban.com/userName' },
+	// rss: { url: '/rss.xml' },
 };
 
 // doc: https://giscus.app
